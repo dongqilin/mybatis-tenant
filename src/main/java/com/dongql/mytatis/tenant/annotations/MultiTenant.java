27722bbdf00e2c13ee8f3dd73e120dfba4c485ad
dongqilin <dongqilin@gmail.com>
2017-06-28 20:21:43 +0800

    Project Init

---
+++ src/main/java/com/dongql/mytatis/tenant/annotations/MultiTenant.java
@@ -0,0 +1,7 @@
+package com.dongql.mytatis.tenant.annotations;
+
+/**
+ * Created by dongqilin on 2017/6/28.
+ */
+public @interface MultiTenant {
+}
---
+++ src/test/java/com/dongql/mybatis/tenant/TenantTest.java
@@ -0,0 +1,7 @@
+package com.dongql.mybatis.tenant;
+
+/**
+ * Created by dongqilin on 2017/6/28.
+ */
+public class TenantTest {
+}
